4d607a8f8dd0f64f8257a8ba985005f0bc52aef2
James <jqmesnz@gmail.com>
2020-03-11 11:28:36 +1300

    Added to serial print information

--- Laser_Tripwire/Laser_Tripwire.ino
+++ Laser_Tripwire/Laser_Tripwire.ino
@@ -2,9 +2,9 @@ int sensorValue = 0;                                        // variable for curr
 int disarmedValue = 0;                                      // variable that records the analogue reading from LDR when laser is disengaged ('disarmed' value)
 int triggerAt = 0;                                          // variable for what LDR reading to 'trigger' tripwire at (Let 'disarmed' value be 500, 'armed' value be max of 1000, triggerAt would be 1000 - 500/2 = 1000 - 250 = 750)
 int testTotal = 0;                                          // iterative variable to record total of (testIterations) readings from LDR when initialising
-const int sensorPin = A0;                                       // LDR 'signal' pin
-const int testIterations = 5;                                   // number of readings to perform when initialising
-const int armedValue = 1000;                                    // maximum reading possible from LDR, therefore reading when laser is 'armed'
+const int sensorPin = A0;                                   // LDR 'signal' pin
+const int testIterations = 5;                               // number of readings to perform when initialising
+const int armedValue = 1000;                                // maximum reading possible from LDR, therefore reading when laser is 'armed'
 
 void setup() {
   Serial.begin(9600);                                       // initialise serial monitor at 9600 baud
@@ -24,10 +24,11 @@ void setup() {
 void loop() {
   sensorValue = analogRead(sensorPin);                      // update (sensorValue) to analogue reading from LDR now that the tripwire is armed
   
-  while (sensorValue >= triggerAt) {                     // until the analogue reading has dropped below 'activation' threshold, do the following
+  while (sensorValue >= triggerAt) {                        // until the analogue reading has dropped below 'activation' threshold, do the following
     sensorValue = analogRead(sensorPin);                    // update (sensorValue)
     Serial.println(sensorValue);                            // print current value to serial monitor (for debugging)
   }
 
-  Serial.println("Triggered");                              // if tripwire is broken, print 'Triggered' to serial monitor
+  Serial.print("Triggered @ ");                              // if tripwire is broken, print 'Triggered' to serial monitor
+  Serial.println(sensorValue);
 }
