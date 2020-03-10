5f432ac9a42a8280d43c0ae1cc897e3efe97bdca
James <jqmesnz@gmail.com>
2020-03-10 21:32:04 +1300

    Added comments

--- Laser_Tripwire/Laser_Tripwire.ino
+++ Laser_Tripwire/Laser_Tripwire.ino
@@ -1,35 +1,33 @@
-int sensorPin = A0;
-int sensorValue = 0;
-int disarmedValue = 0;
-int triggerAt = 0;
-int testTotal = 0;
-const testIterations = 5;
-const armedValue = 1000;
+int sensorValue = 0;                                        // variable for current analogue reading from LDR
+int disarmedValue = 0;                                      // variable that records the analogue reading from LDR when laser is disengaged ('disarmed' value)
+int triggerAt = 0;                                          // variable for what LDR reading to 'trigger' tripwire at (Let 'disarmed' value be 500, 'armed' value be max of 1000, triggerAt would be 1000 - 500/2 = 1000 - 250 = 750)
+int testTotal = 0;                                          // iterative variable to record total of (testIterations) readings from LDR when initialising
+const sensorPin = A0;                                       // LDR 'signal' pin
+const testIterations = 5;                                   // number of readings to perform when initialising
+const armedValue = 1000;                                    // maximum reading possible from LDR, therefore reading when laser is 'armed'
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600);                                       // initialise serial monitor at 9600 baud
 
   // TURN LASER MODULE OFF
 
-  for (int i = 0; i <= (testIterations - 1); i++) {
-    testTotal += analogRead(sensorValue);
+  for (int i = 0; i <= (testIterations - 1); i++) {         // perform the following actions (testIterations) times
+    testTotal += analogRead(sensorValue);                   // add current analogue reading from LDR to existing (testTotal) value
   }
 
-  disarmedValue = testTotal / testIterations;
-  triggerAt = armedValue - (disarmedValue / 2);
+  disarmedValue = testTotal / testIterations;               // set (disarmedValue) equal to average of the (testIterations) tests
+  triggerAt = armedValue - (disarmedValue / 2);             // set (triggerAt) to mid point between 'armed' and 'disarmed' / 'triggered'
 
   // TURN LASER MODULE ON
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  sensorValue = analogRead(sensorPin);
+  sensorValue = analogRead(sensorPin);                      // update (sensorValue) to analogue reading from LDR now that the tripwire is armed
   
-  while (sensorValue >= triggerValue) {
-    sensorValue = analogRead(sensorPin);
-    Serial.println(sensorValue);
+  while (sensorValue >= triggerValue) {                     // until the analogue reading has dropped below 'activation' threshold, do the following
+    sensorValue = analogRead(sensorPin);                    // update (sensorValue)
+    Serial.println(sensorValue);                            // print current value to serial monitor (for debugging)
   }
 
-  Serial.println("Triggered");
+  Serial.println("Triggered");                              // if tripwire is broken, print 'Triggered' to serial monitor
 }
