016630d9c79e72ab342d3d96df239f960fc2f485
James <jqmesnz@gmail.com>
2020-03-10 20:43:15 +1300

    Added basic trigger functionality

--- Laser_Tripwire/Laser_Tripwire.ino
+++ Laser_Tripwire/Laser_Tripwire.ino
@@ -1,13 +1,21 @@
 int sensorPin = A0;
 int sensorValue = 0;
+int initialValue = 0;
+int triggerValue = 0;
+const armedValue = 1000;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  
+  initialValue = analogRead(sensorPin);
+  triggerValue = armedValue - (initialValue / 2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue);
+  while (sensorValue >= triggerValue) {
+    sensorValue = analogRead(sensorPin);
+    Serial.println(sensorValue);
+  }
 }
