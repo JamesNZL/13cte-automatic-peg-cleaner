2ad9b42d8164b367e70b08fa2d006a1e14be8e55
James <jqmesnz@gmail.com>
2020-03-10 20:46:51 +1300

    Increased data set for initial value to include average of 5 values

--- Laser_Tripwire/Laser_Tripwire.ino
+++ Laser_Tripwire/Laser_Tripwire.ino
@@ -4,11 +4,23 @@ int initialValue = 0;
 int triggerValue = 0;
 const armedValue = 1000;
 
+int initial1 = 0;
+int initial2 = 0;
+int initial3 = 0;
+int initial4 = 0;
+int initial5 = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  initial1 = analogRead(sensorPin);
+  initial2 = analogRead(sensorPin);
+  initial3 = analogRead(sensorPin);
+  initial4 = analogRead(sensorPin);
+  initial5 = analogRead(sensorPin);
+  initialValue = (initial1 + initial2 + initial3 + initial4 + initial5) / 5;
   
-  initialValue = analogRead(sensorPin);
   triggerValue = armedValue - (initialValue / 2);
 }
 
