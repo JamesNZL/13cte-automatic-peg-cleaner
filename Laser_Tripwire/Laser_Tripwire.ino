07c9df7df3416b7b4b8e6a00387685062737080f
James <jqmesnz@gmail.com>
2020-03-10 20:40:06 +1300

    Added basic LDR functionality

--- Laser_Tripwire/Laser_Tripwire.ino
+++ Laser_Tripwire/Laser_Tripwire.ino
@@ -1,9 +1,13 @@
+int sensorPin = A0;
+int sensorValue = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
 }
