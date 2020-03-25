7ea9530fae59ec0c44c10b383bf5dac44d725cc2
James <jqmesnz@gmail.com>
2020-03-25 15:03:31 +1300

    Added blink with laser diode

--- Laser_Diode/Laser_Diode.ino
+++ Laser_Diode/Laser_Diode.ino
@@ -1,9 +1,14 @@
+const int laserPin = 13;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(laserPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(laserPin, HIGH);
+  delay(1000);
+  digitalWrite(laserPin, LOW);
+  delay(1000);
 }
