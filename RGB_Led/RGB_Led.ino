a8608f40b8243965626a7a7a4f4e22d6586a5e46
James <jqmesnz@gmail.com>
2020-03-25 15:42:00 +1300

    Added code to control colour

--- RGB_Led/RGB_Led.ino
+++ RGB_Led/RGB_Led.ino
@@ -1,9 +1,17 @@
+const int redPin = 8;
+const int greenPin = 10;
+const int bluePin = 12;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  analogWrite(redPin, 255);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 0);
 }
