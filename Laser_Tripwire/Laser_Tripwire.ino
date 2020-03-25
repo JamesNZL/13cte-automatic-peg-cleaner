98ba79e58e1ea9aca2069bbdc87b4bb99e044581
James <jqmesnz@gmail.com>
2020-03-25 15:10:08 +1300

    Added laser functionality
    Inserted code for laser diode into program

--- Laser_Tripwire/Laser_Tripwire.ino
+++ Laser_Tripwire/Laser_Tripwire.ino
@@ -3,15 +3,17 @@ int disarmedValue = 0;                                      // variable that rec
 int triggerAt = 0;                                          // variable for what LDR reading to 'trigger' tripwire at (Let 'disarmed' value be 500, 'armed' value be max of 1000, triggerAt would be 1000 - 500/2 = 1000 - 250 = 750)
 int testTotal = 0;                                          // iterative variable to record total of (testIterations) readings from LDR when initialising
 const int sensorPin = A0;                                   // LDR 'signal' pin
+const int laserPin = 13;                                    // laser pin
 const int testIterations = 5;                               // number of readings to perform when initialising
 const int armedValue = 1023;                                // maximum reading possible from LDR, therefore reading when laser is 'armed'
 
 void setup() {
   pinMode(sensorPin, INPUT);                                // defines (sensorPin) as an input pin
+  pinMode(laserPin, OUTPUT);                                // defines (laserPin) as an output pin
   
   Serial.begin(9600);                                       // initialise serial monitor at 9600 baud
 
-  // TURN LASER MODULE OFF
+  digitalWrite(laserPin, LOW);                              // disengage laser for preliminary sensor reading
 
   for (int i = 0; i <= (testIterations - 1); i++) {         // perform the following actions (testIterations) times
     testTotal += analogRead(sensorValue);                   // add current analogue reading from LDR to existing (testTotal) value
@@ -20,7 +22,7 @@ void setup() {
   disarmedValue = testTotal / testIterations;               // set (disarmedValue) equal to average of the (testIterations) tests
   triggerAt = armedValue - (disarmedValue / 2);             // set (triggerAt) to mid point between 'armed' and 'disarmed' / 'triggered'
 
-  // TURN LASER MODULE ON
+  digitalWrite(laserPin, HIGH);                             // engage laser tripwire
 }
 
 void loop() {
