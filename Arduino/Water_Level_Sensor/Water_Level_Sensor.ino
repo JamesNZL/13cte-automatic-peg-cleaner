305a934b9b4a81ec55941c18bd77b69b534baad4
James <jqmesnz@gmail.com>
2020-05-14 13:06:02 +1200

    Added serial print for testing

--- Arduino/Water_Level_Sensor/Water_Level_Sensor.ino
+++ Arduino/Water_Level_Sensor/Water_Level_Sensor.ino
@@ -4,9 +4,13 @@ const int waterPin = A0;
 void setup()
 {
 	pinMode(waterPin, INPUT);
+
+	Serial.begin(9600);
 }
 
 void loop()
 {
 	waterValue = analogRead(waterPin);
+
+	Serial.println(waterValue);
 }
