c1d93ff755883f5012fc019dcbbe0fff3c5b8d1f
James <jqmesnz@gmail.com>
2020-05-14 13:59:37 +1200

    Added LED to indicate danger point

--- Arduino/Water_Level_Sensor/Water_Level_Sensor.ino
+++ Arduino/Water_Level_Sensor/Water_Level_Sensor.ino
@@ -3,12 +3,14 @@ unsigned long currentTime;
 unsigned long lastPrint = 0;
 bool waterDanger = false;
 const int waterPin = A0;
+const int ledPin = 13;
 const int minimumValue = 250;
 const unsigned long printDelay = 500;
 
 void setup()
 {
 	pinMode(waterPin, INPUT);
+	pinMode(ledPin, OUTPUT);
 
 	Serial.begin(9600);
 }
@@ -24,15 +26,18 @@ void loop()
 		waterDanger = false;
 	}
 
+	digitalWrite(ledPin, waterDanger);
+
 	if ((currentTime - lastPrint) >= printDelay) {
 	    if (waterDanger == true) {
 	    	Serial.print("Danger @");
 	    } else {
-	    	Serial.print("Safe @");
+	    	Serial.
+	    	print("Safe @");
 	    }
 
 	    Serial.println(waterValue);
-	    
+
 	    lastPrint = currentTime;
 	}
 }
