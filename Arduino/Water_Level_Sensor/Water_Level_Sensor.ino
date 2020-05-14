7c3ed77b8afa6cbbe7d2fae21f42ab7c9134b38a
James <jqmesnz@gmail.com>
2020-05-14 13:44:57 +1200

    Added conditional statements to determine whether current water level is dangerous

--- Arduino/Water_Level_Sensor/Water_Level_Sensor.ino
+++ Arduino/Water_Level_Sensor/Water_Level_Sensor.ino
@@ -1,7 +1,9 @@
 int waterValue = 0;
 unsigned long currentTime;
 unsigned long lastPrint = 0;
+bool waterDanger = false;
 const int waterPin = A0;
+const int minimumValue = 250;
 const unsigned long printDelay = 500;
 
 void setup()
@@ -16,8 +18,21 @@ void loop()
 	waterValue = analogRead(waterPin);
 	currentTime = millis();
 
-	if((currentTime - lastPrint) >= printDelay) {
+	if (waterValue > minimumValue) {
+	    waterDanger = true;
+	} else {
+		waterDanger = false;
+	}
+
+	if ((currentTime - lastPrint) >= printDelay) {
+	    if (waterDanger == true) {
+	    	Serial.print("Danger @");
+	    } else {
+	    	Serial.print("Safe @");
+	    }
+
 	    Serial.println(waterValue);
+	    
 	    lastPrint = currentTime;
 	}
 }
