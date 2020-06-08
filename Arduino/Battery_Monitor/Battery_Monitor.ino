ab7ac2c350c10ef7143e24a09180e84b9e141b54
James <jb16214@my.westlake.school.nz>
2020-06-08 19:11:54 +1200

    Added functionality to convert analogue input to voltage and serial print

--- Arduino/Battery_Monitor/Battery_Monitor.ino
+++ Arduino/Battery_Monitor/Battery_Monitor.ino
@@ -1,12 +1,21 @@
-int batteryValue;
+float batteryValue;
 const int batteryPin = A0;
+const float batteryLow = 3.9;
+const float batteryCritical = 3.7;
+const float readVoltage = 5.0/1023.0;
 
 void setup()
 {
 	pinMode(batteryPin, INPUT);
+
+	Serial.begin(9600);
 }
 
 void loop()
 {
-	batteryValue = analogRead(batteryPin);
+	batteryValue = analogRead(batteryPin) * readVoltage;
+
+	Serial.println(batteryValue);
+
+	delay(100);
 }
